d5109ca288a38eeec9b50801d5df3167c9968c9a
Thabiso Motsoko <ThabisoM@ps.studio>
2020-10-29 19:33:24 +0200

    Add Product Category UI

---
+++ FarmPros.ApplicationLogic/ViewModels/ProductCategoryViewModels/ProductCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FarmPros.ApplicationLogic.ViewModels.ProductCategoryViewModels
+{
+    public class ProductCategoryViewModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid? ParentId { get; set; }
+
+        [Required]
+        [Display(Name = "Product Category")]
+        public string Name { get; set; }
+    }
+}
---
+++ FarmPros/Controllers/ProductCategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FarmPros.ApplicationLogic.ViewModels.ProductCategoryViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmPros.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCategoryViewModel model)
+        {
+            return View("Index");
+        }
+    }
+}
